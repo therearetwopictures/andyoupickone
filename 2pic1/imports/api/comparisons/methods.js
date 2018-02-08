c0d1d44025242dbffe8a6f2b2666cdaea34041e1
Ian <ianhitchcock@outlook.com>
2018-02-08 14:39:11 -0800

    add schema packages and schema for comparisons

--- 2pic1/imports/api/comparisons/methods.js
+++ 2pic1/imports/api/comparisons/methods.js
@@ -4,18 +4,4 @@ import { Meteor } from "meteor/meteor";
 import { check } from "meteor/check";
 import { Comparisons } from "./comparisons.js";
 
-Meteor.methods({
-  "comparisons.insert"(title, url) {
-    check(url, String);
-    check(title, String);
-
-    return Comparisons.insert({
-      url,
-      title,
-      createdAt: new Date()
-    });
-  },
-  "comparisons.addNew"() {
-    return Comparisons.insert({});
-  }
-});
+Meteor.methods({});
