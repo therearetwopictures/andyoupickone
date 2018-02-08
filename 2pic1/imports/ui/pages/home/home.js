9857a00719396df66d0090fc29b274bee8a58625
Scott <scottdlivingstone@gmail.com>
2018-02-08 14:56:33 -0800

    remove scratch work from home.js

--- 2pic1/imports/ui/pages/home/home.js
+++ 2pic1/imports/ui/pages/home/home.js
@@ -2,9 +2,3 @@ import "./home.html";
 
 import "../../components/header/header.js";
 import "../../components/picture/picture.js";
-
-console.log(
-  Meteor.call("dictionary.generateSeedWords", function(err, res) {
-    console.log(res);
-  })
-);
