988186edadd4868866d00016611109eedafb6491
Ian <ianhitchcock@outlook.com>
2018-02-08 17:46:22 -0800

    Fetch random images

--- 2pic1/client/main.js
+++ 2pic1/client/main.js
@@ -4,5 +4,14 @@ import React from "react";
 import AppComponent from "../imports/ui/containers/app/app.js";
 
 Meteor.startup(() => {
+  Meteor.call("comparisons.addOne");
+  Meteor.call("comparisons.addOne");
+  Meteor.call("comparisons.addOne");
+  Meteor.call("comparisons.addOne");
+  Meteor.call("comparisons.addOne");
+  Meteor.call("comparisons.addOne");
+  Meteor.call("comparisons.addOne");
+  Meteor.call("comparisons.addOne");
+  Meteor.call("comparisons.addOne");
   render(<AppComponent />, document.getElementById("app"));
 });
