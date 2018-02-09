82b6a628a218daa9d46d603b22ce22a200fd598a
Scott <scottdlivingstone@gmail.com>
2018-02-09 15:27:27 -0800

    Cookies are now storing user ids now I need to do something useful with them

--- 2pic1/imports/api/userSessions/methods.js
+++ 2pic1/imports/api/userSessions/methods.js
@@ -4,18 +4,24 @@ import { check } from "meteor/check";
 import Users from "./users.js";
 import { Cookies } from "meteor/ostrio:cookies";
 
+newUser = () => {
+  return Users.insert({ init: true }, (err, insert) => {
+    // console.log(insert);
+    return insert;
+  });
+};
+
 Meteor.methods({
   "users.sendCookie"() {
-    const cookies = new Cookies();
-    newUser = () => {
-      Users.insert({ init: true }, (err, insert) => {
-        console.log(insert);
-        cookies.set("userID", insert);
-      });
-    };
-    newUser();
-    console.log(cookies);
-
+    let cookies = new Cookies();
+    WebApp.connectHandlers.use((req, res, next) => {
+      cookies = req.Cookies;
+      // console.log(Cookies);
+      let insert = newUser();
+      // console.log(insert);
+      cookies.set("userID", insert);
+      console.log(cookies);
+    });
     // console.log(cookies);
   }
 });
