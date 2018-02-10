bcdf6812a6b6a088dc3c1d797e9783f5e126ad5f
Ian <ianhitchcock@outlook.com>
2018-02-10 15:08:24 -0800

    refactor helper functions away

--- 2pic1/imports/api/comparisons/methods.js
+++ 2pic1/imports/api/comparisons/methods.js
@@ -1,46 +1,20 @@
 // Methods related to comparisons
 
 import { Meteor } from "meteor/meteor";
-import { check } from "meteor/check";
 import Comparisons from "./comparisons.js";
 import CompMeta from "../compMeta/compMeta";
-import { Dictionary } from "../dictionary/dictionary.js";
 import GoogleImages from "google-images";
 import {
   downloadImage,
   getAWSUrl,
   getUniqueImgNameFromUrl
 } from "../helpers/imageUtils.js";
+import { randNum, searchWords, getUrl } from "../helpers/comparisonUtils";
 
 const imageSearch = new GoogleImages(
   Meteor.settings.googleSearch[0].engineId,
   Meteor.settings.googleSearch[0].apiKey
 );
-
-const searchWords = () =>
-  Dictionary.find(
-    { $or: [{ id: randNum() }, { id: randNum() }, { id: randNum() }] },
-    { fields: { _id: 0, word: 1 } }
-  )
-    .fetch()
-    .map(obj => obj.word)
-    .join(" ") + " imagesize:500x500";
-
-const getUrl = async () => {
-  let url = undefined;
-  let seedWords = undefined;
-  while (!url) {
-    seedWords = searchWords();
-    const imageObj = await imageSearch.search(seedWords);
-    if (imageObj[0]) {
-      url = imageObj[Math.floor(Math.random() * imageObj.length)].url;
-    }
-  }
-  return [url, seedWords.slice(0, -18)];
-};
-
-const randNum = () => Math.floor(Math.random() * 143090).toString();
-
 Meteor.methods({
   "comparisons.getRandOne"(userId = null) {
     Users.find(userID, { compId })[{}];
