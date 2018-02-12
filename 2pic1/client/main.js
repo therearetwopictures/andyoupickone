5de49bf496fdb9cd5411c106613449af18e3ea49
Scott <scottdlivingstone@gmail.com>
2018-02-12 14:41:48 -0800

    Work on updating comp meta with watson results

--- 2pic1/client/main.js
+++ 2pic1/client/main.js
@@ -5,16 +5,16 @@ import AppComponent from "../imports/ui/containers/app/app.js";
 
 Meteor.startup(() => {
   AccountsAnonymous.login();
-  // Meteor.call("userData.createUserSession");
+  Meteor.call("userData.createUserSession");
   // Meteor.call("comparisons.addOne");
   // Meteor.call(
   //   "compMeta.classifyImage",
+  //   "434u3eCWXcRJgmQRv",
   //   "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1437660869i/23745753._UY500_SS500_.jpg",
   //   "http://www.artitude.eu/immagini/news/3-1307111909274.jpg"
   // );
 
   // Meteor.call("comparisons.getRandOne");
 
-  // Meteor.call("users.sendCookie");
   render(<AppComponent />, document.getElementById("app"));
 });
