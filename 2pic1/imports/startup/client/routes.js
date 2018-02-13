9f01bae35fffc02b4420236f5b9c90465131b3fb
Lindsey <lindsey@users-MacBook-Pro.local>
2018-02-12 16:19:59 -0800

    added routes

---
+++ 2pic1/imports/startup/client/routes.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { render } from "react-dom";
+//import Meteor from "meteor/meteor";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AppComponent from "../../ui/containers/app/app.js";
+
+export default Meteor.startup(() => {
+  AccountsAnonymous.login();
+  Meteor.call("userData.createUserSession");
+  // Meteor.call("comparisons.addOne");
+  // Meteor.call(
+  //   "compMeta.classifyImage",
+  //   "434u3eCWXcRJgmQRv",
+  //   "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1437660869i/23745753._UY500_SS500_.jpg",
+  //   "http://www.artitude.eu/immagini/news/3-1307111909274.jpg"
+  // );
+
+  // Meteor.call("comparisons.getRandOne");
+
+  render(
+    <Router>
+      <div>
+        <Switch>
+          {" "}
+          <Route exact path="/" component={AppComponent} />
+          <Route
+            exact
+            path="/:compId"
+            render={({ match }) => (
+              <AppComponent compId={match.params.compId} />
+            )}
+          />
+        </Switch>
+      </div>
+    </Router>,
+
+    document.getElementById("app")
+  );
+});
