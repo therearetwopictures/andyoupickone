990874d836f4dcef3a5e0ed2bc7a7c3e8b24df41
Ian <ianhitchcock@outlook.com>
2018-02-14 11:51:33 -0800

    continue refactor of comparisons methods

--- 2pic1/imports/startup/server/fixtures.js
+++ 2pic1/imports/startup/server/fixtures.js
@@ -9,19 +9,6 @@ Meteor.startup(() => {
     const adminUser = Object.assign({ role: "admin" }, user);
     return adminUser;
   });
-  if (Comparisons.find().count() === 0) {
-    const data = [
-      {
-        urlA:
-          "https://firebasestorage.googleapis.com/v0/b/boomtown-e933c.appspot.com/o/urlA.jpg?alt=media&token=1dbd46ea-bcb5-4b42-a4b5-df6ce1d2ac2e",
-        urlB:
-          "https://firebasestorage.googleapis.com/v0/b/boomtown-e933c.appspot.com/o/urlB.jpg?alt=media&token=8b933e2e-941c-421d-82b6-c01ac7f01453",
-        createdAt: new Date()
-      }
-    ];
-
-    data.forEach(comparison => Comparisons.insert(comparison));
-  }
   if (Meteor.users.find({ role: "admin" }).count() === 0) {
     Accounts.createUser({
       email: "scottdlivingstone@gmail.com",
