8912210defb794cb804bb53d4b73369d87732d32
Scott <scottdlivingstone@gmail.com>
2018-02-15 10:05:48 -0800

    Add unstyled login form to allow admin login with different acccess

--- 2pic1/imports/ui/containers/AdminContainer/AdminContainer.js
+++ 2pic1/imports/ui/containers/AdminContainer/AdminContainer.js
@@ -2,6 +2,7 @@ import { Meteor } from "meteor/meteor";
 import React, { Component } from "react";
 import { watsonBatchClassifyImages } from "./helpers";
 import AccountsUIWrapper from "../../components/AccountsUIWrapper";
+import { ComboBox } from "meteor/universe:accounts-ui";
 import LeaderBoardContainer from "../LeaderBoardContainer/LeaderBoardContainer.js";
 
 export default class AdminContainer extends Component {
@@ -11,18 +12,21 @@ export default class AdminContainer extends Component {
 
   render() {
     return (
-      <div className="watson-functions-container">
-        <h1>HELLLLLLO</h1>
-        <button
-          onClick={e => {
-            e.preventDefault();
-            Meteor.call("comparisons.classifyImage");
-          }}
-        >
-          Generate Tags
-        </button>
-        <LeaderBoardContainer />
-        <button>Generate Tags</button>
+      <div>
+        <ComboBox />
+        <div className="watson-functions-container">
+          <h1>HELLLLLLO</h1>
+          <button
+            onClick={e => {
+              e.preventDefault();
+              Meteor.call("comparisons.classifyImage");
+            }}
+          >
+            Generate Tags
+          </button>
+          <LeaderBoardContainer />
+          <button>Generate Tags</button>
+        </div>
       </div>
     );
   }
