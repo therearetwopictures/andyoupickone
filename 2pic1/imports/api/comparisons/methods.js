e1b8503275f597d8df85889cdff5a8781a19ba03
Scott <scottdlivingstone@gmail.com>
2018-02-15 16:59:22 -0800

    Fix merge conflict

--- 2pic1/imports/api/comparisons/methods.js
+++ 2pic1/imports/api/comparisons/methods.js
@@ -27,12 +27,7 @@ Meteor.methods({
         urlA: 1
       }
     }).fetch();
-<<<<<<< HEAD
-
-    // if (!result[0].A && !result[0].B) Meteor.call("comparisons.addOne");
-=======
     if (!result[0].A && !result[0].B) Meteor.call("comparisons.addOne");
->>>>>>> db7fdebe6c89f0e66bb29e1b36c6c9d68a5bf13d
     return result;
   },
   "comparisons.getRandOne"() {
