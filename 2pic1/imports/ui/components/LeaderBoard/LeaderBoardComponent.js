1fec4cdb20c714a23fbdd181929e32d60f04a6a9
Lindsey <lindsey@users-MBP.hitronhub.home>
2018-02-18 07:35:50 -0800

    connected most popular image query to ui

--- 2pic1/imports/ui/components/LeaderBoard/LeaderBoardComponent.js
+++ 2pic1/imports/ui/components/LeaderBoard/LeaderBoardComponent.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
+import "./styles.css";
 
-const LeaderBoard = () => {
+const LeaderBoard = props => {
   return (
-    <div>
-      <h1>The Leader Board</h1>
-      <div>
+    <div className="flex-container">
+      <h1>GOAT</h1>
+      <div className="flex-item">
+        <h1>Most Popular Image:</h1>
+        <p>Selected {props.mostPopularImage.count} times</p>
+        <img src={props.mostPopularImage.url} />
+      </div>
+      <div className="flex-item">
         <p>
           {" "}
-          Most Clicked Image:
+          Closest to Even Split:
           <button
             onClick={e => {
               e.preventDefault();
-              Meteor.call("comparisons.getMostPopularImage", function(
+              Meteor.call("comparisons.getEvenComparison", function(
                 error,
                 result
               ) {
@@ -26,42 +32,31 @@ const LeaderBoard = () => {
             Find Out!
           </button>
         </p>
-        <p> Closest to Even Split: </p>
-        <button
-          onClick={e => {
-            e.preventDefault();
-            Meteor.call("comparisons.getEvenComparison", function(
-              error,
-              result
-            ) {
-              if (error) {
-                console.log(error.reason);
-                return;
-              }
-              console.log(result);
-            });
-          }}
-        >
-          Find Out!
-        </button>
-        <p> Least Popular Image: </p>
-        <button
-          onClick={e => {
-            e.preventDefault();
-            Meteor.call("comparisons.getLeastPopularImage", function(
-              error,
-              result
-            ) {
-              if (error) {
-                console.log(error.reason);
-                return;
-              }
-              console.log(result);
-            });
-          }}
-        >
-          Find Out!
-        </button>
+      </div>
+      <div className="flex-item">
+        <p>
+          {" "}
+          Least Popular Image:
+          <button
+            onClick={e => {
+              e.preventDefault();
+              Meteor.call("comparisons.getLeastPopularImage", function(
+                error,
+                result
+              ) {
+                if (error) {
+                  console.log(error.reason);
+                  return;
+                }
+                console.log(result);
+              });
+            }}
+          >
+            Find Out!
+          </button>
+        </p>
+      </div>
+      <div className="flex-item">
         <p>
           {" "}
           Most Viewed Comparison:
@@ -83,6 +78,8 @@ const LeaderBoard = () => {
             Find Out!
           </button>
         </p>
+      </div>
+      <div className="flex-item">
         <p>
           {" "}
           Average Session Time:
@@ -104,6 +101,8 @@ const LeaderBoard = () => {
             Find Out!
           </button>
         </p>
+      </div>
+      <div className="flex-item">
         <p>
           {" "}
           Longest Session Time:
@@ -125,9 +124,28 @@ const LeaderBoard = () => {
             Find Out!
           </button>
         </p>
-        <p> User with Most Picks: </p>
+      </div>
+      <div className="flex-item">
         <p>
-          {" "}
+          User with Most Picks:
+          <button
+            onClick={e => {
+              e.preventDefault();
+              Meteor.call("userData.getLeader", function(error, result) {
+                if (error) {
+                  console.log(error.reason);
+                  return;
+                }
+                console.log(result);
+              });
+            }}
+          >
+            Find Out!
+          </button>
+        </p>
+      </div>
+      <div className="flex-item">
+        <p>
           UserId with Most Sessions:
           <button
             onClick={e => {
--- 2pic1/imports/ui/containers/LeaderBoardContainer/LeaderBoardContainer.js
+++ 2pic1/imports/ui/containers/LeaderBoardContainer/LeaderBoardContainer.js
@@ -5,11 +5,34 @@ import LeaderBoard from "../../components/LeaderBoard";
 export default class LeaderBoardContainer extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      mostPopularImage: {}
+    };
   }
+  getMostPopularImage() {
+    Meteor.call("comparisons.getMostPopularImage", (error, result) => {
+      if (error) {
+        console.log(error.reason);
+        return;
+      }
+      const url = result[0].A ? result[0].urlA : result[0].urlB;
+      const count = result[0].A ? result[0].A : result[0].B;
+      let query = {};
+      query["url"] = url;
+      query["count"] = count;
+      this.setState({
+        mostPopularImage: query
+      });
+    });
+  }
+  componentDidMount() {
+    getMostPopularImage();
+  }
+
   render() {
     return (
       <div>
-        <LeaderBoard />
+        <LeaderBoard mostPopularImage={this.state.mostPopularImage} />
       </div>
     );
   }
