6750353af08a01dd1c1c62a7037f8213be053675
Lindsey <lindsey@users-MacBook-Pro.local>
2018-02-18 14:10:39 -0800

    styled goat page, need to style fonts

--- 2pic1/imports/ui/components/Goat/GoatComponent.js
+++ 2pic1/imports/ui/components/Goat/GoatComponent.js
@@ -3,6 +3,7 @@ import "./styles.css";
 
 const GoatComponent = props => (
   <div className="container">
+    <h1>GOAT</h1>
     <div className="item">
       <div className="picture-wrapper">
         <h1>Most Popular Image:</h1>
@@ -19,6 +20,48 @@ const GoatComponent = props => (
         <div className="desc">
           Picked {props.leastPopularImage.count}{" "}
           {props.leastPopularImage.count > 1 ? "times" : "time"}
+        </div>
+      </div>
+    </div>
+    <h1>Most Popular Comparison:</h1>
+    <p>
+      Viewed {props.mostPopularComparison.count}{" "}
+      {props.mostPopularComparison.count > 1 ? "times" : "time"}
+    </p>
+    <div className="item">
+      <div className="picture-wrapper">
+        <img src={props.mostPopularComparison.urlA} />
+        <div className="desc">
+          Picked {props.mostPopularComparison.ACount}{" "}
+          {props.mostPopularComparison.ACount > 1 ? "times" : "time"}
+        </div>
+      </div>
+      <div className="picture-wrapper">
+        <img src={props.mostPopularComparison.urlB} />
+        <div className="desc">
+          Picked {props.mostPopularComparison.BCount}{" "}
+          {props.mostPopularComparison.BCount > 1 ? "times" : "time"}
+        </div>
+      </div>
+    </div>
+    <h1>Most Contentious Comparison:</h1>
+    <p>
+      Viewed {props.closestToEvenSplit.count}{" "}
+      {props.closestToEvenSplit.count > 1 ? "times" : "time"}
+    </p>
+    <div className="item">
+      <div className="picture-wrapper">
+        <img src={props.closestToEvenSplit.urlA} />
+        <div className="desc">
+          Picked {props.closestToEvenSplit.ACount}{" "}
+          {props.closestToEvenSplit.ACount > 1 ? "times" : "time"}
+        </div>
+      </div>
+      <div className="picture-wrapper">
+        <img src={props.closestToEvenSplit.urlB} />
+        <div className="desc">
+          Picked {props.closestToEvenSplit.BCount}{" "}
+          {props.closestToEvenSplit.BCount > 1 ? "times" : "time"}
         </div>
       </div>
     </div>
