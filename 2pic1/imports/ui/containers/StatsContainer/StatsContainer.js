50c5c9011c56700bda3c484163aa9b1bbc7e8901
Scott <scottdlivingstone@gmail.com>
2018-02-18 22:38:43 -0800

    Watson tags are most unfortunately not going to happen tonight

--- 2pic1/imports/ui/containers/StatsContainer/StatsContainer.js
+++ 2pic1/imports/ui/containers/StatsContainer/StatsContainer.js
@@ -12,43 +12,43 @@ class StatsContainer extends Component {
   constructor(props) {
     super(props);
   }
-  getWatsonColorTags = () => {
-    const colorRegex = /color/;
-    this.props.comparisons.reduce((accu, comp) => {
-      if (comp.tagsA) {
-        let compATags = comp.tagsA.reduce((colorTags, tag) => {
-          tagClass = tag.class;
-          if (colorRegex.test(tagClass)) {
-            let tagWords = tagClass.split(" ");
-            if (tagWords[tagWords.length - 2] in colorTags) {
-              colorTags[tagWords[tagWords.length - 2]]++;
-            } else {
-              colorTags[tagWords[tagWords.length - 2]] = 1;
-            }
-          }
-          return colorTags;
-        }, {});
-        if (compATags in accu) {
-        }
-        console.log(compATags);
-      }
+  // getWatsonColorTags = () => {
+  //   const colorRegex = /color/;
+  //   this.props.comparisons.reduce((accu, comp) => {
+  //     if (comp.tagsA) {
+  //       let compATags = comp.tagsA.reduce((colorTags, tag) => {
+  //         tagClass = tag.class;
+  //         if (colorRegex.test(tagClass)) {
+  //           let tagWords = tagClass.split(" ");
+  //           if (tagWords[tagWords.length - 2] in colorTags) {
+  //             colorTags[tagWords[tagWords.length - 2]]++;
+  //           } else {
+  //             colorTags[tagWords[tagWords.length - 2]] = 1;
+  //           }
+  //         }
+  //         return colorTags;
+  //       }, {});
+  //       if (compATags in accu) {
+  //       }
+  //       console.log(compATags);
+  //     }
 
-      if (comp.tagsB) {
-        comp.tagsB.forEach(() => {});
-      }
-      return accu;
-    }, {});
-  };
+  //     if (comp.tagsB) {
+  //       comp.tagsB.forEach(() => {});
+  //     }
+  //     return accu;
+  //   }, {});
+  // };
 
   render() {
-    this.props.comparisons && this.getWatsonColorTags();
+    // this.props.comparisons && this.getWatsonColorTags();
     return (
       <div>
         <InsightsNavBar />
         <h1 className="header">Stats!</h1>
         <Stats />
         <ScatterPlot />
-        {/* <GridFlasher comps={this.props.comparisons} /> */}
+        <GridFlasher comps={this.props.comparisons} />
       </div>
     );
   }
@@ -57,6 +57,6 @@ class StatsContainer extends Component {
 export default withTracker(() => {
   Meteor.subscribe("comparisons.all");
   return {
-    comparisons: Comparisons.find({}).fetch()
+    comparisons: Comparisons.find({}, { fields: { _id: 1 } }).fetch()
   };
 })(StatsContainer);
